b476f24bd94e9ae4af5919fd92b1ec91deca6ef1
Pedro Pivato <pivatopedro@gmail.com>
2024-02-02 01:30:59 -0300

    primeiro commit

---
+++ test/specs/login.spec.js
@@ -0,0 +1,7 @@
+describe('Access Admin Panel', () => {
+    it('Should login with valid credentials', async () => {
+        setTimeout(() => {
+            console.log('abriu')
+        }), 10000
+    });
+});
